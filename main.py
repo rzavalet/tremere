f6acb2bda40dc3f5699668e8b4e815bc79ee357f
rzavalet <rj.zavaleta@gmail.com>
2026-05-26 00:03:41 -0600

    ready set go

---
+++ main.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2026 rzavalet <rzavalet@noemail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+
+if __name__ == '__main__':
+    print('Hello, world!')
